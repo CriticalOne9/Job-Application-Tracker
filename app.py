cddf5ce7895449cf84951c36df0ccf194c203bf0
Uzair <uzzuza365@gmail.com>
2026-09-15 14:43:02 +0530

    Add Initial tracker page

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return "<h1>Job Application Tracker</h1><p>Your project is running.</p>"
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
